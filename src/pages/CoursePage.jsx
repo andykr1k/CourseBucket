ab4dec902287b3a28b100e88a84460c0bf182304
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-01-18 11:48:38 -0800

    Fixed Card UI and Course Page

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import data from '../data/courses.json'
+import { motion } from "framer-motion";
 
 function CoursePage() {
   let { id } = useParams();
@@ -7,16 +8,23 @@ function CoursePage() {
         data.id.toLowerCase().includes(id.toLowerCase())
       );
   return (
-    course && course.length>0 && course.map((item)=>
-      <div className='h-100vh w-100vw p-5 space-y-3'>
+    <div className='h-100vh w-100vw p-5'>
+      <div className="flex mb-3">
+            <div>
+              <a href="/">
+                <motion.button className='font-bold bg-blue-600 p-3 rounded-md' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                  Home
+                </motion.button>
+              </a>
+            </div>
+        </div>
+    {course && course.length>0 && course.map((item)=>
+    <div className="space-y-3 mb-5">
         <div className="w-full flex h-32 p-6 bg-white border border-gray-200 rounded-lg shadow-md justify-between">
           <div>
           <h5 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">{item.name}</h5>
           <p className="font-normal text-gray-700 dark:text-gray-400">{item.id}</p>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.course_time}</p>
-          </div>
-          <div>
-            <a className='font-bold bg-blue-600 p-3 rounded-md' href='/'>Home</a>
           </div>
         </div>
         <div className="flex w-full h-16 p-6 bg-white border border-gray-200 rounded-lg">
@@ -27,8 +35,9 @@ function CoursePage() {
           <p className="font-normal text-gray-600">Discord Links &nbsp;</p>
           <h5 className="font-bold text-gray-900 underline">{item.discord_link}</h5>
         </div>
-      </div>
-    )
+    </div>
+    )}
+    </div>
   )
 }
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import data from '../data/courses.json'
+import { motion } from 'framer-motion';
 
 function SearchPage() {
   const [search, setSearch] = useState('');
@@ -17,7 +18,7 @@ function SearchPage() {
         CourseBucket
       </h1>
 
-    <form className='mx-10 md:mx-48 p-10'>
+    <form className='mx-10 md:mx-48 p-10 pb-8'>
       <label for="default-search" className="mb-2 text-sm font-medium sr-only text-white">Search</label>
       <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -30,21 +31,32 @@ function SearchPage() {
           <button type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Search</button>
       </div>
     </form>
-
-    <div className='grid place-items-center grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 m-10'>
+    <div>
+    { search == ''
+    ?
+      <h1 className="text-2xl md:text-4xl font-normal text-center text-gray-600 pb-5">Explore</h1>
+    :
+      <h1 className="text-2xl md:text-4xl font-normal text-center text-gray-600">Results</h1>
+    }
+    </div>
+    <div className='grid place-items-center grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 m-10 lg:m-36 lg:mt-5 mt-2'>
     { search == ''
     ?
       data && data.length>0 && data.map((item)=>
-      <div className="w-full h-32 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} className="w-full h-36 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div>
+          <a href="#">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.id}</h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.name}</p>
-            <a href={item.id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Learn more
-                <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap overflow-x-auto scrollbar-hide">{item.name}</p>
         </div>
+        <div className=''>
+          <a href={item.id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Learn more
+              <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+        </div>
+        </motion.div>
        )
     :
       sortedCourses && sortedCourses.length>0 && sortedCourses.map((item)=>
