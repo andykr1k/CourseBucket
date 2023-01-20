0cc07b9b50800a88c224f65fcc0d4ccb8e9a37f0
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-01-19 22:22:57 -0800

    Added DB Fetching

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,31 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import data from '../data/courses.json'
 import { motion } from 'framer-motion';
+import { supabase } from '../db/supabase'
 
 function SearchPage() {
   const [search, setSearch] = useState('');
+  const [supadata, setSupadata] = useState([]);
 
+  const fetchCourses = useCallback(async () => {
+    const { data: supadata } = await supabase
+      .from('Courses')
+      .select('*')
+      setSupadata(supadata);
+  });
+
+  console.log(supabase);
+  
   const filteredCourses = data.filter(data =>
     data.name.toLowerCase().includes(search.toLowerCase()) ||
     data.id.toLowerCase().includes(search.toLowerCase())
   );
 
   const sortedCourses = filteredCourses.sort();
+  
+  useEffect(() => {
+    fetchCourses();
+  }, []);
 
   return (
     <div className="">
@@ -19,11 +34,11 @@ function SearchPage() {
       </h1>
 
     <form className='mx-10 md:mx-48 p-10 pb-8'>
-      <label for="default-search" className="mb-2 text-sm font-medium sr-only text-white">Search</label>
+      <label className="mb-2 text-sm font-medium sr-only text-white">Search</label>
       <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
                 </path>
               </svg>
           </div>
@@ -42,16 +57,16 @@ function SearchPage() {
     <div className='grid place-items-center grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 m-10 lg:m-36 lg:mt-5 mt-2'>
     { search == ''
     ?
-      data && data.length>0 && data.map((item)=>
+    supadata && supadata.length>0 && supadata.map((item)=>
       <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} className="w-full h-36 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <div>
           <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.id}</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.course_id}</h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap overflow-x-auto scrollbar-hide">{item.name}</p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap overflow-x-auto scrollbar-hide">{item.course_name}</p>
         </div>
         <div className=''>
-          <a href={item.id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href={item.course_id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Learn more
               <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </a>
