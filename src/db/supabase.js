1861b12a09e70077f9c2343f4d45553e68f2d476
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-01-23 01:10:20 -0800

    Fast Fix

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-
+import {url, key} from '../env'
 // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
-export const supabase = createClient('https://gmosiigapqapgevmioyq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtb3NpaWdhcHFhcGdldm1pb3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxOTE3NDQsImV4cCI6MTk4OTc2Nzc0NH0.5qO6tU8dgopabz_B-Gbr2CORNNGMhKTe23dyH9yGXVQ')
+export const supabase = createClient(url, key)
 
 // export const supabase = createClient(supabaseUrl, supabaseKey)
---
+++ src/env.js
@@ -0,0 +1,2 @@
+export const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtb3NpaWdhcHFhcGdldm1pb3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxOTE3NDQsImV4cCI6MTk4OTc2Nzc0NH0.5qO6tU8dgopabz_B-Gbr2CORNNGMhKTe23dyH9yGXVQ'
+export const url = 'https://gmosiigapqapgevmioyq.supabase.co'
