2349a80d60d6b1ecab6c2770aaba3a0ea724bf1f
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-02-01 11:08:13 -0800

    Fixing UI for smaller devices

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -59,7 +59,6 @@ function SearchPage() {
                 </svg>
             </div>
             <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search Course ID" required onChange={(e) => setSearch(e.target.value)}></input>
-            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Search</button>
         </div>
       </form>
       <div>
@@ -76,18 +75,18 @@ function SearchPage() {
       supadata && supadata.length>0 && supadata.map((item)=>
         <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} className="w-full h-42 p-6 bg-white/90 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div className='overflow-x-hidden'>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.course_id}</h5>
+            <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.course_id}</h5>
             {
               item.course_name.length > 20 
               ?
-              <h5 className="mb-3 text-sm md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap scrollbar-hide">{item.course_name}</h5>
+              <h5 className="mb-3 text-xs md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap scrollbar-hide">{item.course_name}</h5>
               :
-              <h5 className="mb-3 text-sm md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap scrollbar-hide">{item.course_name}</h5>
+              <h5 className="mb-3 text-xs md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap scrollbar-hide">{item.course_name}</h5>
             }
             
           </div>
           <div>
-            <a href={item.course_id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href={item.course_id} className="inline-flex items-center px-2 md:px-3 py-2 text-xs md:text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Learn more
                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
@@ -98,11 +97,11 @@ function SearchPage() {
       sortedCourses && sortedCourses.length>0 && sortedCourses.map((item)=>
           <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} className="w-full h-42 p-6 bg-white/90 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div>
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.course_id}</h5>
-              <p className="mb-3 text-sm md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap overflow-x-auto scrollbar-hide">{item.course_name}</p>
+              <h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.course_id}</h5>
+              <p className="mb-3 text-xs md:text-base font-normal text-gray-700 dark:text-gray-400 whitespace-nowrap scrollbar-hide">{item.course_name}</p>
           </div>
           <div>
-              <a href={item.id} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <a href={item.id} className="inline-flex items-center px-2 md:px-3 py-2 text-xs md:text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Learn more
                   <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </a>
