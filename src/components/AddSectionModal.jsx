b883dcd9cb5b99f40653d5d1b65eb4b363497faf
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-02-08 01:46:23 -0800

    Fixed Modals

--- src/components/AddSectionModal.jsx
+++ src/components/AddSectionModal.jsx
@@ -229,7 +229,7 @@ function AddSectionModal({close, name, id}) {
             </div>
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-          <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-red-800 mt-1" onClick={() => close(false)}>Cancel</button>
+          <button className="text-white bg-red-500 hover:bg-red-700  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-red-800 mt-1" onClick={() => close(false)}>Cancel</button>
         </form>
       </div>
     )
